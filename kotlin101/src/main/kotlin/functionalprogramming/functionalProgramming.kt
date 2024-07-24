9d17d2b65b18702903cbdb4e5f1a71ea04d9a7f9
Li Heng <liheng6567@gmail.com>
2024-07-24 17:07:55 +0800

    Destructuring syntax vs multiple lambda argument syntax

--- kotlin101/src/main/kotlin/functionalprogramming/functionalProgramming.kt
+++ kotlin101/src/main/kotlin/functionalprogramming/functionalProgramming.kt
@@ -10,5 +10,12 @@ fun main() {
     println(increment2(1)) // 2
     println(increment3(1)) // 2
 
-    //TODO 2 arguments
+    //lambda with 2 arguments
+    // parentheses in function type
+    // no parentheses in lambda arguments
+    val sum: (Int, Int) -> Int = { a, b -> a + b }
+    val sum2 = { a: Int, b: Int -> a + b }
+
+    println(sum(1, 2)) // 3
+    println(sum2(1, 2)) // 3
 }
